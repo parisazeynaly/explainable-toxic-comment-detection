3fc8a227e4368b1502be7979bbc4a8bd53159708
parisazeynaly <parisa.zeynaly@gmail.com>
2025-08-17 22:49:53 +0200

    initial

---
+++ comment.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+train_df = pd.read_csv("Data/train.csv")
+print(train_df.head())
+
+test_df=pd.read_csv("Data/test.csv")
+print(test_df.head())
+print(train_df['toxic'].value_counts())
